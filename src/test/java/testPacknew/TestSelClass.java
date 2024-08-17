baef39f70539a3559f35eef3b0e44f6f2e290fe9
admin <admin@mail.com>
2024-08-17 22:34:00 +0530

    test

--- src/test/java/testPacknew/TestSelClass.java
+++ src/test/java/testPacknew/TestSelClass.java
@@ -3,12 +3,14 @@ package testPacknew;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+@Test
 public class TestSelClass {
 
-	public static void main(String[] args) {
+	public void intiateBrowser() {
 		WebDriverManager.edgedriver().setup();
 		WebDriver driver=new EdgeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
